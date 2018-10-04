adfd9f3f7c5bec068d3af3791899f7b6030d6deb
tony <2452588075@qq.com>
2018-10-04 10:52:32 +0800

    添加内容

--- demo.sh
+++ demo.sh
@@ -2,3 +2,6 @@ mkdir hi
 cd hi
 mkdir  css js 
 touch css/style.css  js/main.js  index.html
+echo "<!DOCTYPE><title>Hello</title><h1>Hi</h1>" > index.html
+echo  "h1{color: red;}" >  css/style.css
+echo   "var string = "Hello World" alert(string)" > js/main.js
